9c96331a84cd466c870bb6faa8c067c787b7cc9c
Brustar <yeals2@gmail.com>
2017-02-22 18:00:13 +0800

    UDP

--- Udp.lua
+++ Udp.lua
@@ -9,6 +9,8 @@ function Udp:create()
 
     local udp = {}
     
+    self.timer = nil
+    
     function udp.connect()
 	   C4:CreateNetworkConnection (UDP_CONNECT_ID, "255.255.255.255")
 	   C4:NetConnect(UDP_CONNECT_ID, UDP_PORT, 'UDP')
@@ -18,13 +20,14 @@ function Udp:create()
 	   if (strStatus == "ONLINE") then
 		  local a,b,c,d = string.match(C4:GetControllerNetworkAddress(),"(%d+).(%d+).(%d+).(%d+)")
 		  local pack = Pack:create(UDP_CMD,tonumber(Properties["masterID"]),tonumber(a),tonumber(b),tonumber(c),tonumber(d),SERVER_PORT,UDP_CMD)
-		  C4:SetTimer(5 * 1000, function(timer, skips)
+		  self.timer = C4:SetTimer(5 * 1000, function(timer, skips)
 			 C4:SendToNetwork(UDP_CONNECT_ID, UDP_PORT, pack:hex())
 		  end,true)
 	   end
     end
     
     function udp.stop()
+	   self.timer:Cancel()
 	   C4:NetDisconnect(UDP_CONNECT_ID, UDP_PORT, 'UDP')
     end
     
