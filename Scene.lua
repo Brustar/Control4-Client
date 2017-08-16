8cef7686cb372134503d82c9063d47795189aff1
Brustar <yeals2@gmail.com>
2017-08-16 15:02:10 +0800

    sch

--- Scene.lua
+++ Scene.lua
@@ -5,21 +5,26 @@ function Scene.start(data)
     for i,v in ipairs(data.devices) do
 	   local deviceid = tonumber(v.enumber,16)
 	   local deviceType = tonumber(v.htype,16)
+	   local device = Device:create()
 	   --switch
-	   if v.isPoweron == 1 or v.poweron == 1 or v.swithon == 1 or v.unlock == 1 or v.pushing == 1 or v.showed == 1 or v.waiting == 1 then
+	   if v.isPoweron == 1 or v.poweron == 1 or v.swithon == 1 or v.unlock == 1 or v.pushing == 1 or v.showed == 1 or v.waiting == 1 or v.openvalue > 0 then
 		  print("on:",deviceid)
-		  local device = Device:create()
 		  if deviceType == device.BGMUSIC then
 			 device:bgMusicON(deviceid,C4:RoomGetId())
 		  elseif deviceType == device.AMPLIFIER then
 			 device:switchAmplifier(deviceid,v.waiting)
 		  else
 			 C4:SendToDevice(deviceid,"ON",{})
+			 if deviceType == device.DVD then
+				C4:SendToDevice(deviceid,"PLAY",{})
+			 end
 		  end
 	   end
 	   
-	   if v.isPoweron == 0 or v.poweron == 0 or v.swithon == 0 or v.pushing == 0 or v.showed == 0 or v.waiting == 0 then
-		  if deviceType == device.AMPLIFIER then
+	   if v.isPoweron == 0 or v.poweron == 0 or v.swithon == 0 or v.pushing == 0 or v.showed == 0 or v.waiting == 0 or v.openvalue == 0 then
+		  if deviceType == device.BGMUSIC then
+			 C4:SendToDevice(C4:RoomGetId(),"STOP",{})
+		  elseif deviceType == device.AMPLIFIER then
 			 device:switchAmplifier(deviceid,v.waiting)
 		  else
 			 C4:SendToDevice(deviceid,"OFF",{})
