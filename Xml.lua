6aa23ad8ff1c47df99f05f482b98170037d33362
Brustar <yeals2@gmail.com>
2017-10-10 14:17:55 +0800

    xml

--- Xml.lua
+++ Xml.lua
@@ -43,14 +43,12 @@ function Xml:create(config)
 	   local scenes={}
 	   
 	   local str = C4:GetProjectItems("AGENTS","NO_ROOT_TAGS")
+	   local pattern = "<name>Advanced Lighting</name>\n?<type>9</type>\n?<itemdata>.-</itemdata>\n?<state>([^\n]+)</state>"
 	   
-	   local pattern = "<name>Advanced Lighting</name>\n?<type>9</type>\n?<itemdata><large_image>[^\n]+</large_image><small_image>[^\n]+</small_image></itemdata>\n?<state>([^\n]+)</state>"
 	   local tVers = C4:GetVersionInfo()
 	   local strVers = tVers["version"]
 	   local major, minor, _, _ = string.match(strVers, "(%d+)\.(%d+)\.(%d+)\.(%d+)")
-	   if tonumber(major) == 2 and tonumber(minor) > 8 then
-		  pattern = "<name>Advanced Lighting</name>\n?<type>9</type>\n?<itemdata><small_image>[^\n]-</small_image><large_image>[^\n]-</large_image></itemdata>\n?<state>([^\n]-)</state>"
-	   end
+
 	   local state = string.match(str,pattern)
 	   state = state:gsub("&lt;","<"):gsub("&gt;",">")
 	   for name,id in string.gmatch(state,"<AdvScene>%s*<name>(.-)</name>[%d%D]-<scene_id>(%d+)</scene_id>") do
